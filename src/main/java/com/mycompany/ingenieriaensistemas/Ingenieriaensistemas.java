b49de4d0559dd88e32b33903060f78b094c40e85
ConnyMoreno <connymoreno25@gmail.com>
2024-11-26 15:54:05 -0500

    mi primer commit

---
+++ src/main/java/com/mycompany/ingenieriaensistemas/Ingenieriaensistemas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ingenieriaensistemas;
+
+/**
+ *
+ * @author ASOCIACIÒN
+ */
+public class Ingenieriaensistemas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
