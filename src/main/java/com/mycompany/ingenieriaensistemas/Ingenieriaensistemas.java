d2b365affb8023f80a4fa70ab501a42ca5c786cb
ConnyMoreno <connymoreno25@gmail.com>
2024-11-26 16:01:32 -0500

    modificar

--- src/main/java/com/mycompany/ingenieriaensistemas/Ingenieriaensistemas.java
+++ src/main/java/com/mycompany/ingenieriaensistemas/Ingenieriaensistemas.java
@@ -11,6 +11,6 @@ package com.mycompany.ingenieriaensistemas;
 public class Ingenieriaensistemas {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("bienvenidos a la facultad de sistemas");
     }
 }
